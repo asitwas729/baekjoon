61c58244ac9494700a16d014d0da62bd61d26f69
Yang Hyemin <asitwas729@gmail.com>
2022-12-26 19:45:02 +0900

    baejoon 3.5 yet buffer사용을 할 수 있는데 배열이랑 같이 쓰는 법을 아직 모르겠음

---
+++ loop/src/fast.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class fast {
+  public static void main(String[] args){
+    BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    StringTokenizer st = null;
+
+    try {
+      int T = Integer.parseInt(bf.readLine());
+      for(int i = 0; i < T; i++){
+        st = new StringTokenizer(bf.readLine());
+        bw.write((Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken())) + "\n");
+        bw.close();
+      }
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+}
