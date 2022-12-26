94ce34f3ac7cac46ed5fc1724ea03299be3c30d0
Yang Hyemin <asitwas729@gmail.com>
2022-12-26 20:09:09 +0900

    baejoon #7287

---
+++ sprout/src/My.java
@@ -0,0 +1,8 @@
+public class My {
+  public static void main(String[] args){
+    int solved = 26;
+    String BOJid = "asitwas729";
+    System.out.println(solved);
+    System.out.println(BOJid);
+  }
+}
